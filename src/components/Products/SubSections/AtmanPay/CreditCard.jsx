6ac196ef3ce68ae3abc551d318fdb101b0a52124
“gzuppa” <gzuppa86@gmail.com>
2024-10-25 17:18:17 -0600

    feat: credit card section

--- src/components/Products/SubSections/AtmanPay/CreditCard.jsx
+++ src/components/Products/SubSections/AtmanPay/CreditCard.jsx
@@ -39,7 +39,7 @@ const CreditCard = () => {
             </p>
             </div>
         </div>
-        <div className="grid gap-8 row-gap-10 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid gap-8 row-gap-10 sm:grid-cols-1 lg:grid-cols-3">
             <div>
             <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                 <svg
@@ -218,7 +218,7 @@ const CreditCard = () => {
             </div>
             <h6 className="mb-2 font-semibold leading-5 text-gray-900 dark:text-white">Transferencias internacionales</h6>
             <p className="mb-3 text-md text-gray-900 dark:text-white">
-                Liquidación de remesas a otros países
+                Liquidación de remesas a otros países más fácil que nunca
             </p>
             <ul className="mb-4 -ml-1 space-y-2 text-gray-900 dark:text-white">
                 <li className="flex items-start">
@@ -332,7 +332,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                INE
+                <p className='text-xs'>INE</p>
                 </li>
                 <li className="flex items-start">
                 <span className="mr-1">
@@ -350,7 +350,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                Comprobante de domicilio
+                <p className='text-xs'>Comprobante de domicilio</p>
                 </li>
                 <li className="flex items-start">
                 <span className="mr-1">
@@ -368,7 +368,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                CURP
+                <p className='text-xs'>CURP</p>
                 </li>
                 <li className="flex items-start">
                 <span className="mr-1">
@@ -386,7 +386,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                Nombre, fecha y país de nacimiento
+                <p className='text-xs'>Nombre, fecha y país de nacimiento</p>
                 </li>
                 <li className="flex items-start">
                 <span className="mr-1">
@@ -404,7 +404,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                Correo electrónico
+                <p className='text-xs'>Correo electrónico</p>
                 </li>
                 <li className="flex items-start">
                 <span className="mr-1">
@@ -422,95 +422,7 @@ const CreditCard = () => {
                     />
                     </svg>
                 </span>
-                Teléfono celular
-                </li>
-            </ul>
-            </div>
-            <div>
-            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-                <svg
-                className="w-7 h-7 text-deep-purple-accent-400"
-                strokeLinecap="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                >
-                <path
-                    d="M8,13l3,3,9.379-9.379a2.122,2.122,0,0,0,0-3h0a2.122,2.122,0,0,0-3,0Z"
-                    fill="none"
-                    stroke="currentColor"
-                />
-                <polyline
-                    fill="none"
-                    points="10 11 10 6 6 2 3 5 7 9"
-                    stroke="currentColor"
-                />
-                <polyline
-                    fill="none"
-                    points="15 12 15 17 19 21 22 18 18 14"
-                    stroke="currentColor"
-                />
-                <path d="M8,23a7,7,0,0,1-7-7" fill="none" stroke="currentColor" />
-                <path d="M8,19a3,3,0,0,1-3-3" fill="none" stroke="currentColor" />
-                </svg>
-            </div>
-            <h6 className="mb-2 font-semibold leading-5 text-gray-900 dark:text-white">First contact</h6>
-            <p className="mb-3 text-sm text-gray-900 dark:text-white">
-                For those who have seen the Earth from space or in dreams.
-            </p>
-            <ul className="mb-4 -ml-1 space-y-2 text-gray-900 dark:text-white">
-                <li className="flex items-start">
-                <span className="mr-1">
-                    <svg
-                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                    >
-                    <polygon
-                        strokeWidth="4"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        fill="none"
-                        points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                    </svg>
-                </span>
-                Listen
-                </li>
-                <li className="flex items-start">
-                <span className="mr-1">
-                    <svg
-                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                    >
-                    <polygon
-                        strokeWidth="4"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        fill="none"
-                        points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                    </svg>
-                </span>
-                Communicate
-                </li>
-                <li className="flex items-start">
-                <span className="mr-1">
-                    <svg
-                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                    >
-                    <polygon
-                        strokeWidth="4"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        fill="none"
-                        points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                    </svg>
-                </span>
-                Run
+                <p className='text-xs'>Teléfono celular</p>
                 </li>
             </ul>
             </div>
