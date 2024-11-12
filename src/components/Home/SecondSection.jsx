32c53019e40831f868a8a573ad7837223bf5b001
“gzuppa” <gzuppa86@gmail.com>
2024-11-12 12:46:01 -0600

    feat: change points

--- src/components/Home/SecondSection.jsx
+++ src/components/Home/SecondSection.jsx
@@ -7,7 +7,7 @@ const SecondSection = () => {
             <div class="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
                 <div class="text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 class="animate-fade-left mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Revolucionamos el mercado financiero digital en México</h2>
-                    <p class="animate-fade-left mb-8 font-light lg:text-xl">Contamos con una plataforma tecnológica de punta, transaccional de fácil acceso y uso</p>
+                    <p class="animate-fade-left mb-8 font-light lg:text-xl">Contamos con una plataforma tecnológica de punta, transaccional de fácil acceso y uso.</p>
                 </div>
                 <div class='image-container'>
                     <img class="bottom w-64" src="https://res.cloudinary.com/gusironhack/image/upload/v1731436775/atphone.png"/>
