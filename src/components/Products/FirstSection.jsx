652e90202358d7d8ebc5f0ccaec92759b5879033
“gzuppa” <gzuppa86@gmail.com>
2025-03-05 19:46:58 -0600

    fix: products

--- src/components/Products/FirstSection.jsx
+++ src/components/Products/FirstSection.jsx
@@ -14,15 +14,12 @@ const FirstSection = () => {
           </h1>
           <div class="lg:px-40">
             <p class="mt-4 mb-12 text-center text-gray-900 dark:text-white text-md">
-              El objetivo de ATMAN es convertirse en el líder del sector Fintech
-              a través del portafolio de servicios y productos financieros
-              digitales que permita maximizar el volumen de negocios y su
-              rentabilidad.
+              El objetivo de GARC es convertirse en el líder del sector Fintech a través del portafolio de servicios y productos financieros digitales que permita maximizar el volumen de negocios y su rentabilidad.
             </p>
           </div>
         </Stack>
         <Stack>
-          <div class="md:px-4 md:grid md:grid-cols-1 lg:grid-cols-4 lg:px-20 gap-5 space-y-">
+          <div class="md:px-4 md:grid md:grid-cols-1 lg:grid-cols-3 lg:px-60 gap-5 space-y-">
             <div class="bg-white px-12 lg:px-6 pt-6 lg:pt-10 pb-6 lg:pb-0 mb-8 lg:mb-0 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
               <div class="relative">
                 <img
@@ -66,12 +63,32 @@ const FirstSection = () => {
               <div class="relative">
                 <img
                   class="w-full h-80 rounded-xl"
+                  src="https://res.cloudinary.com/gusironhack/image/upload/v1741225366/moneyexchange_ramqap.jpg"
+                  alt="Colors"
+                />
+              </div>
+              <h1 class="mt-4 text-gray-800 text-xl font-bold cursor-pointer text-center">
+                Money Exchange<br></br>
+                <br />
+              </h1>
+              <div class="my-4">
+                <NavLink to={"/productos/atman-pay"}>
+                  <button class="mt-4 text-sm w-full text-white bg-principalAmber py-2 rounded-xl shadow-lg">
+                    Conoce más
+                  </button>
+                </NavLink>
+              </div>
+            </div>
+            <div class="bg-white px-12 lg:px-6 pt-6 lg:pt-10 pb-6 lg:pb-0 mb-8 lg:mb-0 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+              <div class="relative">
+                <img
+                  class="w-full h-80 rounded-xl"
                   src="https://res.cloudinary.com/gusironhack/image/upload/v1730138175/Atman/product2.png"
                   alt="Colors"
                 />
               </div>
               <h1 class="mt-4 text-gray-800 text-xl font-bold cursor-pointer text-center">
-                ATMAN Pay<br></br>
+                GARC Pay<br></br>
                 <br />
               </h1>
               <div class="my-4">
@@ -91,11 +108,31 @@ const FirstSection = () => {
                 />
               </div>
               <h1 class="mt-4 text-gray-800 text-xl font-bold cursor-pointer text-center">
-                Tarjeta de crédito<br></br>
+                GARC Credit card<br></br>
                 <br />
               </h1>
               <div class="my-4">
                 <NavLink to={"/productos/tarjeta"}>
+                  <button class="mt-4 text-sm w-full text-white bg-principalAmber py-2 rounded-xl shadow-lg">
+                    Conoce más
+                  </button>
+                </NavLink>
+              </div>
+            </div>
+            <div class="bg-white px-12 lg:px-6 pt-6 lg:pt-10 pb-6 lg:pb-0 mb-8 lg:mb-0 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+              <div class="relative">
+                <img
+                  class="w-full h-80 rounded-xl"
+                  src="https://res.cloudinary.com/gusironhack/image/upload/v1741225441/carleasing_x9jf5q.jpg"
+                  alt="Colors"
+                />
+              </div>
+              <h1 class="mt-4 text-gray-800 text-xl font-bold cursor-pointer text-center">
+                Leasing & Factoring<br></br>
+                <br />
+              </h1>
+              <div class="my-4">
+                <NavLink to={"/productos/atman-pay"}>
                   <button class="mt-4 text-sm w-full text-white bg-principalAmber py-2 rounded-xl shadow-lg">
                     Conoce más
                   </button>
